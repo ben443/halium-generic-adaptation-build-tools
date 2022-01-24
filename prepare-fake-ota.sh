96badaf79344e0a88bfe460ecf573b65bbeb0944
TheKit <nekit1000@gmail.com>
2022-01-24 23:49:34 +0200

    prepare-fake-ota.sh: update ROOTFS_URL for focal

--- prepare-fake-ota.sh
+++ prepare-fake-ota.sh
@@ -5,9 +5,9 @@ source "${HERE}/deviceinfo"
 
 # Fetches android9 rootfs and generic system image to prepare flashable image from CI-built device tarball
 URL='https://system-image.ubports.com'
-ROOTFS_URL='https://ci.ubports.com/job/xenial-hybris-android9-rootfs-arm64/lastSuccessfulBuild/artifact/ubuntu-touch-android9-arm64.tar.gz'
+ROOTFS_URL='https://ci.ubports.com/job/focal-hybris-rootfs-arm64/job/master/lastSuccessfulBuild/artifact/ubuntu-touch-android9plus-rootfs-arm64.tar.gz'
 DEVICE_GENERIC_URL='https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_arm64/job/halium-10.0/lastSuccessfulBuild/artifact/halium_halium_arm64.tar.xz'
-OTA_CHANNEL='16.04/arm64/android9/devel'
+OTA_CHANNEL='20.04/arm64/android9plus/devel'
 
 if [[ "$deviceinfo_bootimg_os_version" == 9* ]]; then
     DEVICE_GENERIC_URL='https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_arm64/job/main/lastSuccessfulBuild/artifact/halium_halium_arm64.tar.xz'
