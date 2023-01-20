74cda38236b3adc35d091cb8d8519902d2939bec
azkali <a.ffcc7@gmail.com>
2023-01-19 23:30:20 -0500

    make-android-images.sh: fix naming, convert sparse to raw image, don't move system image as it is useless

--- make-android-images.sh
+++ make-android-images.sh
@@ -15,7 +15,7 @@ if [ ! -d "$HALIUM" ]; then
 	repo init -u https://github.com/Halium/android -b $deviceinfo_android_branch --depth=1
 	repo sync -j$(nproc)
 	git clone https://gitlab.azka.li/l4t-community/ubtouch/manifest.git --recursive -b $deviceinfo_android_branch .repo/local_manifests
-	wget -O ./halium/devices/nintendo_icosa_sr.xml https://gitlab.azka.li/l4t-community/ubtouch/manifest/-/raw/$deviceinfo_android_branch/default.xml
+	wget -O ./halium/devices/${deviceinfo_manufacturer}_${deviceinfo_android_target}.xml https://gitlab.azka.li/l4t-community/ubtouch/manifest/-/raw/$deviceinfo_android_branch/default.xml
 	./halium/devices/setup $deviceinfo_android_target
 	./.repo/local_manifests/snack/snack.sh -y -p -w
 	hybris-patches/apply-patches.sh --mb
@@ -26,9 +26,9 @@ else
 fi
 
 # HACK: replace defconfig by deviceinfo one
-sed -i 's/TARGET_KERNEL_CONFIG.*$/TARGET_KERNEL_CONFIG := '$deviceinfo_kernel_defconfig'/g' ./device/$deviceinfo_manufacturer/$deviceinfo_android_target/BoardConfig.mk
+sed -i 's/TARGET_KERNEL_CONFIG.*$/TARGET_KERNEL_CONFIG := '$deviceinfo_kernel_defconfig'/g' device/$deviceinfo_manufacturer/$deviceinfo_android_target/BoardConfig.mk
 
-# Prepare 
+# Prepare
 source build/envsetup.sh
 breakfast $deviceinfo_android_target
 
@@ -36,7 +36,4 @@ breakfast $deviceinfo_android_target
 mka e2fsdroid
 mka systemimage
 mka vendorimage
-
-mv "$HALIUM/out/target/product/$deviceinfo_android_target/vendor.img" \
-	"$HALIUM/out/target/product/$deviceinfo_android_target/system.img" \
-	"$TMPDOWN/partitions"
+simg2img "$HALIUM/out/target/product/$deviceinfo_android_target/vendor.img" "$TMPDOWN/partitions/vendor.img"
